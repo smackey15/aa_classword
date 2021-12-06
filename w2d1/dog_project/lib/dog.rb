40e1add96eb023f6c13405b6bdb648c4a66804d0
sean <midruss@yahoo.com>
2021-12-06 13:50:55 -0500

    first dog code

---
+++ w2d1/dog_project/lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    end
+    
+    def breed 
+        @breed
+    end
+    
+    def age
+        @name
+    end
+end
